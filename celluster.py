161ad1dd5de6650c430ef98eb6c98b5be4d3f009
Matt Hodgman <matthodg1@gmail.com>
2021-07-29 14:43:47 -0600

    implemented iterative consensus clustering

--- celluster.py
+++ celluster.py
@@ -1,47 +1,35 @@
 import argparse
 import pandas as pd
-from itertools import combinations
-import csv
-from operator import itemgetter
+from collections import Counter
 import numpy as np
 
-
-'''
-Cluster class
-'''
-class Cluster:
-
-    # constructor
-    def __init__(self, assignments, cells):
-        self.assignments = assignments # a tuple where each element is the cluster assignment of a different method 
-        self.cells = cells # a list of cell ID's in this cluster
-        self.consensus_scores = {} # a dictionary where keys are other clusters and values are the consensus score between them
-
-    # compare this cluster to another to calculate a consensus score
-    def addComparison(self, cluster):
-        num_methods = len(self.assignments) # the number of clustering methods/algorithms used
-        consensus_score = 0 # a score of what fraction of all methods agreed on cluster assignments
-
-        # compare all cluster assignments and keep score
-        for i in range(num_methods):
-            if self.assignments[i] == cluster.assignments[i]:
-                consensus_score += 1
-
-        # only add consensus score if it isn't zero
-        if consensus_score != 0:
-            consensus_score = consensus_score / num_methods # calculate consensus score NOTE: does this math work correctly or not because I'm dividing ints?
-            self.consensus_scores[cluster.assignments] = consensus_score # add comparison to consensus score sdict
-            
 
 '''
 Parse arguments.
 '''
 def parseArgs():
-    parser = argparse.ArgumentParser(description='Consensus clustering from multiple methods used in MCMICRO to cluster cell types.')
-    parser.add_argument('-i', '--input', help='Input cell cluster assignment files.', nargs='*', action="store", dest="input", required=True)
+    parser = argparse.ArgumentParser(description='Implementation of the consensus clustering method proposed in https://www.sciencedirect.com/science/article/pii/S131915781930597X.')
+    parser.add_argument('-i', '--input', help='Input cell cluster assignment files.', nargs='*', action='store', dest='input', required=True)
     parser.add_argument('-o', '--output', help='The directory to which output files will be saved.', type=str, required=False)
+    parser.add_argument('-c', '--id', help='The name of the column that contains the item ID.', type=str, required=True)
+    parser.add_argument('-d', '--data', help='The orignal file that was used for clustering.', type=str, required=False)
+    parser.add_argument('-t', '--tab', help='Flag to indicate that input files are tab delimited.', action='store_true', required=False)
+    parser.add_argument('-v', '--verbose', help='Flag to print information about the consensus clustering.', action='store_true', required=False)
+    parser.add_argument('-z', '--outliers', help='Flag to keep outliers in output file under the cluster label: 0.', action='store_true', required=False)
+    parser.add_argument('-k', '--clusters', help='The file that contains cluster labels from previous iterations.', type=str, required=False)
+    parser.add_argument('-n', '--name', help='The name of this dataset for output file naming consistency.', type=str, required=False)
+    parser.add_argument('-r', '--iter', help='The iteration number.', type=str, required=False)
     args = parser.parse_args()
     return args
+
+
+'''
+Get input data file name
+'''
+def getDataName(path):
+    fileName = path.split('/')[-1] # get filename from end of input path
+    dataName = fileName[:fileName.rfind('.')] # get data name by removing extension from file name
+    return dataName
 
 
 '''
@@ -55,7 +43,7 @@ def getHeader(file):
     f.close()
 
     # get the input file column headers as list
-    header_columns = header.split(',')
+    header_columns = header.split(delimiter)
 
     return header_columns
 
@@ -66,7 +54,7 @@ Look at first line of an in input file (the header of the csv) to assess if it i
 def validInput(file):
 
     # definition of the columns needed to constitute a valid input file
-    NEEDED_COLUMNS = [CELLID, CLUSTER, METHOD]
+    NEEDED_COLUMNS = [id, CLUSTER, METHOD]
 
     input_columns = getHeader(file) # get input file header columns
     valid = all(col in input_columns for col in NEEDED_COLUMNS) # check if all needed columns are present in input columns list
@@ -79,7 +67,7 @@ Read file into dataframe.
 '''
 def getData(file):
 
-    data = pd.read_csv(file, delimiter=',', index_col=CELLID) # load data from input csv into dataframe
+    data = pd.read_csv(file, delimiter=delimiter, index_col=id) # load data from input csv into dataframe
     method = data[METHOD].iloc[0] # get method name from first row (this is assuming at all rows are from the same method) NOTE: we may not want to assume this!!
     data = data.drop(METHOD, axis=1) # drop method column
     
@@ -87,45 +75,159 @@ def getData(file):
 
 
 '''
-Reformat consensus clusters dict to a list of tuples where each tuple is a cell-cluster assignment.
+Compute the jaccard index of two clusters (sets of samples).
 '''
-def formatConsensusClusters(consensus_clusters):
-
-    ccc_assignments = [] # short for cell consensus cluster assignments
-    cluster_assignment = 0 # arbitrary int to identify different clusters
-
-    # iterate through dict
-    for cluster,cells in consensus_clusters.items():
-        for cell in cells:
-            ccc_assignments.append((cell,cluster_assignment))
-        cluster_assignment += 1
-
-    # sort
-    ccc_assignments = sorted(ccc_assignments, key=itemgetter(0))
-
-    return ccc_assignments
+def jaccard_index(cluster1, cluster2):
+    i = len(cluster1.intersection(cluster2))
+    u = len(cluster1.union(cluster2))
+    jaccard_index = i / u
+    return jaccard_index
 
 
 '''
-Get cluster assignments that have 100% agreement between methods.
-Return a dict where the key is a tuple with the cluster assignments for each method and the value is a list of cell IDs
+Get the method that has the fewest (min) clusters. If there is a tie, just use the method whose column comes first
 '''
-def getConsensusClusters(cluster_table):
-    cluster_table = cluster_table[cluster_table.duplicated(keep=False)] # drop rows that aren't duplicates (duplicate rows mean that there are at least 2 cells that have were assigned to the same clusters across all algorithms)
-    consensus_clusters = cluster_table.groupby(list(cluster_table.columns), as_index=False).groups # dictionary where the key is the cluster assignments of all datasets and the value is a list of all the cells that received those assignments
+def getMinMethod(cluster_table):
+    min = len(cluster_table) # start with the min at the number of cells
+    min_method = '' # the method with the fewest clusters
 
-    return consensus_clusters
+    # iterate through methods 
+    for method in cluster_table.columns:
+        num_clusters = len(pd.unique(cluster_table[method])) # calucate the number of clusters
+        if num_clusters < min: # if min, update values
+            min_method = method
+            min = num_clusters
+
+    return min_method
 
 
 '''
-Write cell consensus cluster assignments to csv
+Get clustering method re-labeling key
 '''
-def writeCSV(ccc_assignments):
-    with open('cell_consensus_cluster_assignments.csv','w') as f:
-        csv_out = csv.writer(f)
-        csv_out.writerow([CELLID,CLUSTER])
-        for row in ccc_assignments:
-            csv_out.writerow(row)
+def getNewLabels(min_clusters_table, m_clusters_table):
+    label_key = {} # a dict where the key is the cluster label in the 'm' method and the value is the cluster label in the 'min' method that has the highest jaccard index
+
+    # lists of cluster labels in each method
+    m_clusters = pd.unique(m_clusters_table)
+    min_clusters = pd.unique(min_clusters_table)
+
+    for c_label1 in m_clusters:
+        indices = {}
+        for c_label2 in min_clusters:
+            c_label1_items = set(m_clusters_table[m_clusters_table == c_label1].index)
+            c_label2_items = set(min_clusters_table[min_clusters_table == c_label2].index)
+            i = jaccard_index(c_label1_items, c_label2_items)
+            indices[i] = c_label2
+        max_index = max(indices.keys()) # find the largest jaccard index
+        label_match =  indices[max_index] # get the label in the min method with that max jaccard index
+        label_key[c_label1] = label_match # add to key dict
+    
+    return label_key
+
+
+'''
+Get the cluster label that has the majority vote
+'''
+def vote(labels):
+    c = Counter(labels) # count the most common label
+
+    # check for majority
+    label, count = c.most_common()[0] # get most common label and its count
+    freq = count / len(labels) # the frequency of the most common label
+
+    if freq > 0.5:
+        return label # return mode label if it has majority
+    else:
+        return np.nan # if it is a tie return NaN
+
+
+'''
+Consensus cluster by matching clusters between methods and relabeling them appropriately. Items without majority vote for cluster label are outliers.
+'''
+def consensusCluster(cluster_table):
+
+    # compare all methods against the one with the least number of clusters
+    min_method = getMinMethod(cluster_table) # get the method with the fewest number of clusters to compare all others against
+    methods = list(cluster_table.columns) # get list of methods
+    methods.remove(min_method) # remove min method because we will not compare it against itself
+
+    for m in methods:
+        label_key = getNewLabels(cluster_table[min_method], cluster_table[m]) # a dict where the key is the cluster label in the 'm' method and the value is the cluster label in the 'min' method that has the highest jaccard index
+        cluster_table[m] = cluster_table[m].map(label_key).fillna(cluster_table[m]) # relabel clusters FASTER
+
+    # vote
+    consensus_labels = [vote(row) for row in cluster_table.to_numpy()] # a list of clusters labels (or nan) ordered for each item ID in cluster_table
+    cluster_table[CLUSTER] = consensus_labels # add results to table
+
+
+'''
+If a previous consensus cluster file is provided, update it to contain the latest iteration item cluster labels
+'''
+def updateConsensusClusters(file):
+    prev_clusters = pd.read_csv(file, delimiter=delimiter, index_col=id) # get previous consensus cluster labels for items
+    max = prev_clusters[CLUSTER].max() # get max cluster label
+    min = cluster_table[CLUSTER].min() # get min cluster label from latest iteration
+
+    # increment latest cluster labels appropriately (might start at 0 or 1?)
+    offset = abs(min - max) # find the difference between the last max and the latest min
+    increment = offset + 1 # set increment so that the min cluster label in the latest iteration becomes the next label from the previous ones
+    cluster_table[CLUSTER] = cluster_table[CLUSTER] + increment # increment latest cluster labels
+    if args.verbose:
+        print(f'This iteration cluster labels were incremented by {increment}')
+
+    clusters_to_add = cluster_table[CLUSTER].dropna().to_frame()
+
+    # update previous cluster labels table
+    # if latest items are in prev_clusters, update cluster label, otherwise append rows
+    contains_all = all(elem in list(prev_clusters.index) for elem in list(cluster_table.index))
+    if contains_all:
+        prev_clusters.update(clusters_to_add)
+    else:
+        constains_any = any(item in list(prev_clusters.index) for item in list(cluster_table.index))
+        if not constains_any:
+            prev_clusters = prev_clusters.append(clusters_to_add).sort_index()
+        else:
+            print('ERROR. I have not coded this yet.')
+
+    # write cluster labels as ints
+    prev_clusters[CLUSTER] = prev_clusters[CLUSTER].astype(int)
+
+    return prev_clusters
+    
+
+'''
+Get and write the input data of outlier items that need to be re-consensus clustered in the next iteration.
+'''
+def writeOutlierData(recluster_ids):
+    # get items that need to be reclustered
+    if args.data != None and len(recluster_ids) > 0:
+        data = pd.read_csv(args.data, delimiter=delimiter, index_col=id)
+        recluster_data = data.loc[recluster_ids]
+
+    # write data to recluster
+    if args.data != None and len(recluster_ids) > 0:
+        recluster_data.to_csv(f'{output}/{data_prefix}-iter-outliers.{extension}', sep=delimiter)
+        if args.verbose:
+            print(f'Items to recluster are in the file: {output}/{data_prefix}-iter-outliers.{extension}')
+
+
+'''
+Format consensus clustered items for output
+'''
+def writeConsensusClusters(cluster_table):
+    # write item cluster labels
+    if args.outliers:
+        cluster_table[CLUSTER] = cluster_table[CLUSTER] + 1 # increment cluster labels
+        cluster_table[CLUSTER] = cluster_table[CLUSTER].map({np.nan : 0}).fillna(cluster_table[CLUSTER]) # set outliers to label '0'
+        if args.verbose:
+            print('Outlier items have the cluster label: 0')
+    else:
+        cluster_table = cluster_table.dropna()
+
+    cluster_table[CLUSTER] = cluster_table[CLUSTER].astype(int)
+    cluster_table.to_csv(f'{output}/{cluster_prefix}-iter-consensus.{extension}', sep=delimiter, columns=[CLUSTER])
+    if args.verbose:
+        print(f'Consensus cluster labels are in the file: {output}/{cluster_prefix}-iter-consensus.{extension}')
 
 
 '''
@@ -134,12 +236,36 @@ Main.
 if __name__ == '__main__':
 
     # constants
-    CELLID = 'CellID' # the header of the cell ID column
     CLUSTER = 'Cluster' # the header of the cluster assignmnet column
     METHOD = 'Method' # the header of the method column
 
     # parse arguments
     args = parseArgs()
+
+    id = args.id # the header of the sample/cell/drug/item ID column
+
+    # whether to read and write csv or tsv
+    if args.tab:
+        delimiter = '\t'
+        extension = 'tsv'
+    else:
+        delimiter = ','
+        extension = 'csv'
+
+    # get iteration number if provided
+    if args.iter != None:
+        iter = '-' + args.iter
+
+    # get prefix for output files
+    if args.name == None:
+        cluster_prefix = getDataName(args.input[0]) + iter
+    else:
+        cluster_prefix = args.name + iter
+    if args.data != None:
+        if args.name == None:
+            data_prefix = getDataName(args.data) + iter
+        else:
+            data_prefix = args.name + iter
 
     # get user-defined output dir (strip last slash if present) or set to current
     if args.output is None:
@@ -148,6 +274,9 @@ if __name__ == '__main__':
         output = args.output[:-1]
     else:
         output = args.output
+
+    if args.verbose:
+        print('Reading files...')
 
     # if input file has correct format, make a matrix from it
     cluster_table = pd.DataFrame() # a df where cell ID is the index and each column is the cluster assignment from a different algorithm
@@ -158,40 +287,33 @@ if __name__ == '__main__':
         else:
             print(f'{file} is incorrectly formatted.')
 
+    if args.verbose:
+        print('Performing consensus clustering...')
 
-    consensus_clusters = getConsensusClusters(cluster_table)
+    # perform consensus clustering
+    consensusCluster(cluster_table)
 
-    # make cluster objects
-    clusters = []
-    for cluster,cells in consensus_clusters.items():
-        clusters.append(Cluster(cluster, cells))
+    if args.verbose:
+        print('Retrieving outliers...')
 
-    # compare all clusters against each other
-    all_comparisons = list(combinations(clusters, 2))
-    for comparison in all_comparisons:
-        comparison[0].addComparison(comparison[1])
-        comparison[1].addComparison(comparison[0])
+    # get item ID's that need to be re-clustered in the next iteration
+    recluster_ids = cluster_table[cluster_table[CLUSTER].isnull()].index
 
+    # update running consensus clusters file
+    if args.clusters != None:
+        complete_cluster_table = updateConsensusClusters(args.clusters)
+        complete_cluster_table.to_csv(f'{output}/{cluster_prefix}-running-consensus.{extension}', sep=delimiter, columns=[CLUSTER])
+        if args.verbose:
+            print(f'Wrote running consensus clusters to: {output}/{cluster_prefix}-running-consensus.{extension}')
+    
+    if args.verbose:
+        print('Writing output files...')
 
-    # for cluster in clusters:
-    #     print(f'{cluster.assignments}\tNum Cells: {len(cluster.cells)}\t Num Comparisons: {len(cluster.consensus_scores.keys())}')
+    writeOutlierData(recluster_ids) # write outlier data
+    writeConsensusClusters(cluster_table) # write consensus cluster labels
 
-    # print(f'Num Clusters: {len(clusters)}')
-
-
-    # import matplotlib.pyplot as plt
-    # num_cells = [len(c.cells) for c in clusters]
-    # num_comps = [len(c.consensus_scores) for c in clusters]
-    # plt.scatter(num_cells,num_comps)
-    # plt.show()
-
-
-    # print(f'{clusters[2].assignments}')
-    # for c, score in clusters[2].consensus_scores.items():
-    #     print(f'{c}:\t{score}')
-
-
-    # ccc_assignments = formatConsensusClusters(consensus_clusters)
-
-    # write to csv
-    # writeCSV(ccc_assignments)
+    # final stats
+    if args.verbose:
+        print('Done.')
+        print(f'{len(cluster_table.index)} ({(len(cluster_table.index) / (len(recluster_ids) + len(cluster_table.index))) * 100:.2f}%) items clustered into {len(pd.unique(cluster_table[CLUSTER]))} clusters.')
+        print(f'{len(recluster_ids)} ({(len(recluster_ids) / (len(recluster_ids) + len(cluster_table.index))) * 100:.2f}%) outlier items to be re-clustered in next iteration.')
